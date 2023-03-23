8554e010473e16561c05b1c4ad674c2a91ce4535
cablebIll <115678147+cablebIll@users.noreply.github.com>
2023-03-24 01:18:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-userChoice = input('for ascii to unicode type "a"\nfor unicode to ascii type "b"\n') #ask user to select which conversion
+userChoice = input('for ascii to unicode type "a"\nfor unicode to ascii type "b"\n')
 if userChoice == 'a' or userChoice == 'A':
-    inputString = input('type the string you want converted:\n') #specific instructions for each conversion
+    inputString = input('type the string you want converted:\n') 
 elif userChoice == 'b' or userChoice == 'B':
     inputString = input('type the string you want converted with each codepoint seperated by a space\ne.g. 97 98 99 100\n') 
-    #specific instructions for each conversion
+    
 def convert():
-    if userChoice == 'a' or userChoice == 'A':#if user selected ascii to unicode do...
-        unicodeString = '' #initialize string for converted unicode
-        for i in inputString: #do process for each value in string
-            i = ord(i) #do the conversion
-            unicodeString += ' ' #add a space between each value in the conversion for clarity
-            unicodeString += str(i) #add converted value to the output string
-        return unicodeString    #return output string to convert() function
-    elif userChoice == 'b' or userChoice == 'B': #if user selected unicode to ascii do...
-        inputList = inputString.split() #initialize list containing each value user wants converted
-        ASCIIlist = [] #initailize string that will contain converted data
-        for i in inputList: #for each entry in list do...
-            if i.isnumeric() == False: #check if the value is a number
-                print('invaid input') #provide error message
-                quit() #quit the program
-            if int(i) not in range(1,1114111): #check if the value is in the convertable range
-                print('invalid input') #provide error message.
-                quit() #quit
-            ASCIIlist.append(chr(int(i))) #add converted value to output list
-            ASCIIstring = ' '.join(ASCIIlist)#create a string with the values of the output list
-        return ASCIIstring #return output string to the convert() function
-    else: #if user did not select a or b do this...
-        print('please select a valid option')# provide error message
-        quit() #quit program
+    if userChoice == 'a' or userChoice == 'A':
+        unicodeString = '' 
+        for i in inputString: 
+            i = ord(i) 
+            unicodeString += ' ' 
+            unicodeString += str(i) 
+        return unicodeString    
+    elif userChoice == 'b' or userChoice == 'B': 
+        inputList = inputString.split() 
+        ASCIIlist = [] 
+        for i in inputList: 
+            if i.isnumeric() == False: 
+                print('invaid input') 
+                quit() 
+            if int(i) not in range(1,1114111): 
+                print('invalid input') 
+                quit()
+            ASCIIlist.append(chr(int(i))) 
+            ASCIIstring = ' '.join(ASCIIlist)
+        return ASCIIstring 
+    else: 
+        print('please select a valid option')
+        quit() 
     
 convert()
 
